828fcef2446c33dcbbea09e7e89b47165afaf99f
Sagarjainjain <sagarjain.jain10@gmail.com>
2023-09-01 17:23:25 +0530

    add_contact

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,15 +3,16 @@ import "./footer.css"
 
 const Footer = () => {
   return (
-    <div className='footer_container'>
-        <div className='footer_container-title'>
-            <h1>Bharati Jewellers</h1>
-        </div>
-        <div className='footer_container-version_control'>
-            <p>Version 1.0</p>
-        </div>
+    <div className="footer_container">
+      <div className="footer_container-title">
+        <h1>Bharati Jewellers</h1>
+        <a href="https://wa.link/3y867k">Contact us - 9833231301</a>
+      </div>
+      <div className="footer_container-version_control">
+        <p>Version 1.0</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer
